8ae3c10e8f6e08482379f9887fac26d551172241
JJs23 <yjh502@nate.com>
2026-03-27 00:03:44 +0900

    chore: bump racemo to v0.0.2

--- Casks/racemo.rb
+++ Casks/racemo.rb
@@ -1,14 +1,13 @@
 cask "racemo" do
-  version "0.0.1"
-  sha256 "5fc119febb6dbc3c700d35abac24d537f40e714946450c998f329f732cb18ee8"
+  version "0.0.2"
+  sha256 "3f7d889d174f6f5f9eb7e7f7ec20209b94f5a126887e446ae3f6a7a0683d329c"
 
-  url "https://github.com/racemo-dev/racemo-releases/releases/download/v#{version}/Racemo_#{version}_aarch64.dmg"
+  url "https://github.com/racemo-dev/racemo-client/releases/download/v#{version}/Racemo_#{version}_Mac_universal.dmg"
   name "Racemo"
   desc "Desktop-native terminal multiplexer built with Rust and Tauri"
   homepage "https://github.com/racemo-dev/racemo"
 
   depends_on macos: ">= :ventura"
-  depends_on arch: :arm64
 
   app "Racemo.app"
 
