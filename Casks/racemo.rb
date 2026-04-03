50b4bbf0e9bf13c107f595c7e8e0283e4a7c9348
JJs23 <yjh502@nate.com>
2026-04-03 18:16:45 +0900

    chore: bump racemo to v0.0.3

--- Casks/racemo.rb
+++ Casks/racemo.rb
@@ -2,12 +2,12 @@ cask "racemo" do
   version "0.0.3"
 
   on_arm do
-    sha256 "c7c8bb36135bf7dbecc9ceeed8ee07dd5c770b20d50a9e70bf4430bfe70a2a5a"
+    sha256 "c7f52786678972c98625f6416a9b09f7b2553cb36ab255bc2117f03c67811045"
     url "https://github.com/racemo-dev/racemo-client/releases/download/v#{version}/Racemo_#{version}_Mac_aarch64.dmg"
   end
 
   on_intel do
-    sha256 "3c23c95c1787423afeb656b58b0bce76cd3c7e76920ea38500e936e2de7dc057"
+    sha256 "00e89c17f461020d821d0eb565dc88e5a2d5f490039d6f5a7112419e863e923e"
     url "https://github.com/racemo-dev/racemo-client/releases/download/v#{version}/Racemo_#{version}_Mac_x64.dmg"
   end
 
